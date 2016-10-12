90cc89e77acc955ab345a5d087c4cdf2a15d7635
Fernando Silva <fernando.debrando@gmail.com>
2016-10-12 19:14:55 -0300

    alteração de configurações de envio de e-mail

--- Dump_ReservaSala.sql
+++ Dump_ReservaSala.sql
@@ -2,7 +2,7 @@
 --
 -- Host: 127.0.0.1    Database: reservasala
 -- ------------------------------------------------------
--- Server version	5.7.13
+-- Server version	5.7.14
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -140,7 +140,7 @@ CREATE TABLE `users` (
   `updated_at` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `users_email_unique` (`email`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -162,4 +162,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2016-06-26 17:56:19
+-- Dump completed on 2016-09-04 18:23:31
